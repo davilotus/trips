b6e88e6df4a256b7653ee5f76fa3399b25cfc97f
Davidson <vader@assoweb.com.br>
2022-04-21 21:10:57 -0300

    Renomenando arquivo style do LinkWrapper e corrigindo arquivo de estilos global

--- src/components/LinkWrapper/styles.ts
+++ src/components/LinkWrapper/styles.ts
@@ -2,18 +2,18 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   position: fixed;
-  z-index 1100;
+  z-index: 1100;
   color: var(--white);
   top: var(--small);
   right: var(--small);
   cursor: pointer;
 
-  svg{
-    transition: color .3s ease;
+  svg {
+    transition: color 0.3s ease;
   }
 
-  &:hover{
-    svg{
+  &:hover {
+    svg {
       color: var(--highlight);
     }
   }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,7 +30,7 @@ const GlobalStyles = createGlobalStyle`
 
   .container{
     padding: 0 15px;
-    max-width: 1170px
+    max-width: 1170px;
     width: 100%;
     margin: 0 auto;
   }
